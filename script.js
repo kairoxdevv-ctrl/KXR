d2cb9b88765360cb08fe9364c1d6cdc6c335e0ee
Kairox <kairoxdevv@gmail.com>
2026-03-18 09:22:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-console.log("NIKKI");
+const btn = document.getElementById("ctaBtn");
 
-// particles
-for(let i=0;i<80;i++){
-const p=document.createElement("span");
-p.style.left=Math.random()*100+"vw";
-p.style.animationDuration=5+Math.random()*10+"s";
-document.getElementById("particles").appendChild(p);
+if (btn) {
+  btn.addEventListener("click", () => {
+    alert("Du klickade 🚀");
+  });
 }
+
+window.addEventListener("load", () => {
+  console.log("Sidan laddad");
+});
