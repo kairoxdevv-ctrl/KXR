fe9472aa13f36708af41e4889b5f9e08ae93dca1
Kairox <kairoxdevv@gmail.com>
2026-03-19 13:02:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-const btn = document.getElementById("ctaBtn");
 
-if (btn) {
-  btn.addEventListener("click", () => {
-    alert("Du klickade 🚀");
-  });
-}
-
-window.addEventListener("load", () => {
-  console.log("Sidan laddad");
-});
